ebf1ce0124a88bf6ed5c553f1cf1a72e828be534
Alexandre Silva <xande1231231@hotmail.com>
2019-05-19 18:18:51 -0300

    Fix

--- pages/home/index.php
+++ pages/home/index.php
@@ -13,7 +13,7 @@
 for($i = 1; $i <= 10; $i++){
 ?>
 
-<href data-href="/?ler&idmanga=<?php echo $i;?>"><div class="manga_a"></div></href>
+<href data-href="/?ler&idmanga=<?php echo $i;?>"><div class="manga_a" style="background-image: url('/pdf/486f88e5889422c72ab7b12a58f794721554828665_full.jpg');"></div></href>
 
 <?php } ?>
 
